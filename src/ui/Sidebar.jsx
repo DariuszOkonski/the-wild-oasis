f87ce92e1c711420de9647d48752255bae3094ee
Dariusz Okoński <darek200180@gmail.com>
2025-07-22 07:26:44 +0200

    add images to public folder

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import Logo from './Logo';
+import MainNav from './MainNav';
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -7,7 +9,12 @@ const StyledSidebar = styled.aside`
   grid-row: 1 / -1;
 `;
 function Sidebar() {
-  return <StyledSidebar>sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
