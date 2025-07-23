c7894640562057932b2dd6f3cf6e2b8a5a15069e
Dariusz Okoński <darek200180@gmail.com>
2025-07-23 07:39:01 +0200

    install supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://utycmysejhoantpdpzxa.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0eWNteXNlamhvYW50cGRwenhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxNjMxMDIsImV4cCI6MjA2ODczOTEwMn0.cfP9pk6XFUMbPacaVK6AjT3Hgd4ERumLFe878Fl5s9Q';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
