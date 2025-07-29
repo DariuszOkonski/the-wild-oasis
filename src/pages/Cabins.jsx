195feccfe799565723351d0d624705bbdd1de80f
Dariusz Okoński <darek200180@gmail.com>
2025-07-29 06:37:18 +0200

    install date-fns v2

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,20 +1,20 @@
-import { useEffect } from 'react';
+import React from 'react';
+import CabinTable from '../features/cabins/CabinTable';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabins';
 
 function Cabins() {
-  useEffect(() => {
-    getCabins()
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
-    <Row type='horizontal'>
-      <Heading as='h1'>All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <React.Fragment>
+      <Row type='horizontal'>
+        <Heading as='h1'>All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </React.Fragment>
   );
 }
 
